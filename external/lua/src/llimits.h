c0440e827f85da904df7af8714a1da1e33039e2f
Konrad Witaszczyk <konrad.witaszczyk@cl.cam.ac.uk>
2022-02-03 15:21:45 +0000

    Cast integer values for pointers via intptr_t.
    Use intptr_t to cast integer values representing pointers to
    capabilities that can be dereferenced.
    
    Clang generated warnings for these issues:
    warning: cast from provenance-free integer type to pointer type will
    give pointer that can not be dereferenced [-Wcheri-capability-misuse]

--- external/lua/src/llimits.h
+++ external/lua/src/llimits.h
@@ -132,7 +132,7 @@ typedef LUAI_UACINT l_uacInt;
 #define cast_char(i)	cast(char, (i))
 #define cast_charp(i)	cast(char *, (i))
 #define cast_sizet(i)	cast(size_t, (i))
-
+#define cast_intptrt(i)	cast(intptr_t, (i))
 
 /* cast a signed lua_Integer to lua_Unsigned */
 #if !defined(l_castS2U)
