386a364fc65aa3d2285318b9dbdaa6da0a6d78fc
Konrad Witaszczyk <konrad.witaszczyk@cl.cam.ac.uk>
2022-02-17 13:03:51 +0000

    Define ABI and ALTABI for aarch64c and riscv64c.
    We considered "aarch64:64:{c,cheri}", "riscv:64:{c,cheri}:hf" and
    "riscv:64:hf:{c,cheri}" for ALTABIs. We decided to use the ":cheri"
    segment instead of ":c" as the choice of CheriABI is fundamental and
    should be emphasised. Additionaly, the ":cheri" segment should be
    placed before a floating-point ABI as it has larger impact on a
    resulting ABI.
    
    Discussed with:	brooks jhb jrtc27

--- libpkg/pkg_elf.c
+++ libpkg/pkg_elf.c
@@ -825,7 +825,7 @@ pkg_get_myarch_elfparse(char *dest, size_t sz, struct os_info *oi)
 	int fd, i;
 	int ret = EPKG_OK;
 	const char *arch, *abi, *endian_corres_str, *wordsize_corres_str, *fpu;
-	bool checkroot;
+	bool checkroot, cheriabi;
 	struct os_info loi;
 
 	const char *abi_files[] = {
@@ -835,6 +835,7 @@ pkg_get_myarch_elfparse(char *dest, size_t sz, struct os_info *oi)
 	};
 
 	arch = NULL;
+	cheriabi = false;
 
 	if (oi == NULL) {
 		memset(&loi, 0, sizeof(loi));
@@ -1002,6 +1003,15 @@ pkg_get_myarch_elfparse(char *dest, size_t sz, struct os_info *oi)
 		    ":%s:%s:%s:%s:%s", arch, wordsize_corres_str,
 		    endian_corres_str, abi, fpu);
 		break;
+	case EM_AARCH64:
+#if defined(EF_AARCH64_CHERI_PURECAP)
+		if (elfhdr.e_flags & EF_AARCH64_CHERI_PURECAP) {
+			cheriabi = true;
+		}
+#endif
+		snprintf(dest + strlen(dest), sz - strlen(dest), ":%s:%s%s",
+		    arch, wordsize_corres_str, cheriabi ? ":cheri" : "");
+		break;
 	case EM_MIPS:
 		/*
 		 * this is taken from binutils sources:
@@ -1044,8 +1054,14 @@ pkg_get_myarch_elfparse(char *dest, size_t sz, struct os_info *oi)
 				abi = "unknown";
 				break;
 		}
-		snprintf(dest + strlen(dest), sz - strlen(dest), ":%s:%s:%s",
-		    arch, wordsize_corres_str, abi);
+#if defined(EF_RISCV_CHERIABI)
+		if (elfhdr.e_flags & EF_RISCV_CHERIABI) {
+			cheriabi = true;
+		}
+#endif
+		snprintf(dest + strlen(dest), sz - strlen(dest),
+		    ":%s:%s%s:%s",
+		    arch, wordsize_corres_str, cheriabi ? ":cheri" : "", abi);
 		break;
 #endif
 	case EM_PPC:
